eb12a7b8d3dd7670e1627da91ef5b7c044b9fd8c
Harun <ximharun@gmail.com>
2026-02-05 20:56:13 +0000

    fixed user authentication to use user_id as primary kery

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    protected $primaryKey = 'user_id';
     protected $fillable = [
     'username',
     'email',
